4f2e2229a358fa5ada97c0da53cd3520b3327c3e
Santiago Rubio Montero <srubiomontero@gmail.com>
2024-06-07 17:12:34 +0200

    changin weapons 2.0

--- PracticaIngenieria/Assets/Scripts/Character/EquipScript.cs
+++ PracticaIngenieria/Assets/Scripts/Character/EquipScript.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class EquipScript : MonoBehaviour
 {
-    public Transform PlayerTransform;
-    public GameObject Gun;
+    public Transform PlayerTransform1;
+    public Transform PlayerTransform2;
+    public GameObject Gun1;
+    public GameObject Gun2;
     public Camera Camera;
-    public float range = 2f;
+    public float range = 1f;
     public float open = 100f;
 
     // Start is called before the first frame update
     void Start()
     {
-        Gun.GetComponent<Rigidbody>().isKinematic = true;
+        Gun1.GetComponent<Rigidbody>().isKinematic = true;
+        Gun2.GetComponent<Rigidbody>().isKinematic = true;
     }
 
     // Update is called once per frame
@@ -21,7 +24,12 @@ public class EquipScript : MonoBehaviour
     {
         if (Input.GetKeyDown("f"))
         {
-            UnequipObject();
+            UnequipObject1();
+            Shoot();
+        }
+        if (Input.GetKeyDown("g"))
+        {
+            UnequipObject2();
             Shoot();
         }
     }
@@ -36,23 +44,44 @@ public class EquipScript : MonoBehaviour
             Target target = hit.transform.GetComponent<Target>();
             if (target != null)
             {
-                EquipObject();
+                EquipObject1();
+                EquipObject2();
             }
         }
     }
 
-    void UnequipObject()
+    void UnequipObject1()
     {
-        PlayerTransform.DetachChildren();
-        Gun.transform.eulerAngles = new Vector3(Gun.transform.eulerAngles.x, Gun.transform.eulerAngles.y, Gun.transform.eulerAngles.z - 45);
-        Gun.GetComponent<Rigidbody>().isKinematic = false;
+        PlayerTransform1.DetachChildren();
+        Gun1.transform.eulerAngles = new Vector3(Gun1.transform.eulerAngles.x, Gun1.transform.eulerAngles.y, Gun1.transform.eulerAngles.z - 45);
+        Gun1.GetComponent<Rigidbody>().isKinematic = false;
     }
 
-    void EquipObject()
+    void UnequipObject2()
     {
-        Gun.GetComponent<Rigidbody>().isKinematic = true;
-        Gun.transform.position = PlayerTransform.transform.position;
-        Gun.transform.rotation = PlayerTransform.transform.rotation;
-        Gun.transform.SetParent(PlayerTransform);
+        PlayerTransform2.DetachChildren();
+        Gun2.transform.eulerAngles = new Vector3(Gun2.transform.eulerAngles.x, Gun2.transform.eulerAngles.y, Gun2.transform.eulerAngles.z - 45);
+        Gun2.GetComponent<Rigidbody>().isKinematic = false;
     }
+
+    void EquipObject1()
+    {
+        Gun1.GetComponent<Rigidbody>().isKinematic = true;
+        Gun1.transform.position = PlayerTransform1.transform.position;
+        Gun1.transform.rotation = PlayerTransform1.transform.rotation;
+        Gun1.transform.SetParent(PlayerTransform1);
+        Gun1.SetActive(true);
+        Gun2.SetActive(false);
+    }
+
+    void EquipObject2()
+    {
+        Gun2.GetComponent<Rigidbody>().isKinematic = true;
+        Gun2.transform.position = PlayerTransform2.transform.position;
+        Gun2.transform.rotation = PlayerTransform2.transform.rotation;
+        Gun2.transform.SetParent(PlayerTransform2);
+        Gun1.SetActive(false);
+        Gun2.SetActive(true);
+    }
+
 }
